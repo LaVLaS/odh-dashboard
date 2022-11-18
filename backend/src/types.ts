174239c77a00608872da14560dadf7c01f3eb734
Chris Chase <cchase@redhat.com>
2022-11-18 12:50:02 -0500

    Fix incorrect type for kube

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -192,7 +192,6 @@ export type KfDefResource = K8sResourceCommon & {
 export type KubeStatus = {
   currentContext: string;
   currentUser: User;
-  currentToken: string;
   namespace: string;
   userName: string | string[];
   clusterID: string;
@@ -209,6 +208,7 @@ export type KubeDecorator = KubeStatus & {
   batchV1Api: k8s.BatchV1Api;
   customObjectsApi: k8s.CustomObjectsApi;
   rbac: k8s.RbacAuthorizationV1Api;
+  currentToken: string;
 };
 
 export type KubeFastifyInstance = FastifyInstance & {
